6b45fe372fce4744c8c12e6f8cfa2ac3d3e87aaa
larousso <adelegue@hotmail.com>
2020-06-26 18:35:31 +0200

    Upgrade to ZIO RC21

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -5,6 +5,7 @@ import router.Routes
 import play.api.routing.Router
 import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext, LoggerConfigurator}
 import commons.AppContext
+import zio.ZManaged.ReleaseMap
 import zio._
 
 class AppLoader extends ApplicationLoader {
@@ -21,14 +22,18 @@ package object modules {
 
   class AppComponentsInstances(context: Context) extends BuiltInComponentsFromContext(context) with AssetsComponents {
     import users._
+    import zio.interop.catz._
 
-    private val reservation: Reservation[Any, Nothing, ZLayer[ZEnv, Throwable, AppContext]] =
-      Runtime.default.unsafeRun(AppContext.live.memoize.reserve)
+    implicit val runtime: Runtime[ZEnv] = Runtime.default
 
-    private implicit val appContext: ZLayer[zio.ZEnv, Throwable, AppContext] =
-      Runtime.default.unsafeRun(reservation.acquire)
+    type Eff[A] = ZIO[ZEnv, Throwable, A]
 
-    applicationLifecycle.addStopHook(() => Runtime.default.unsafeRunToFuture(reservation.release(Exit.unit)))
+    private val memoize: ZManaged[Any, Nothing, ZLayer[ZEnv, Throwable, AppContext]] = AppContext.live.memoize
+
+    private implicit val (appContext: ZLayer[zio.ZEnv, Throwable, AppContext], release: Eff[Unit]) =
+      Runtime.default.unsafeRun(memoize.toResource[Eff].allocated)
+
+    applicationLifecycle.addStopHook(() => Runtime.default.unsafeRunToFuture(release))
 
     override def router: Router = new Routes(httpErrorHandler, new UserController(controllerComponents), assets)
 
--- build.sbt
+++ build.sbt
@@ -8,11 +8,14 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.13.1"
 
 libraryDependencies ++= Seq(
-  "org.iq80.leveldb"         % "leveldb"                              % "0.12",
-  "dev.zio"                  %% "zio"                                 % "1.0.0-RC18-2",
-  "dev.zio"                  %% "zio-interop-cats"                    % "2.0.0.0-RC12",
+  "org.iq80.leveldb"       % "leveldb"             % "0.12",
+  "org.typelevel"          %% "cats-effect"        % "2.1.3",
+  "dev.zio"                %% "zio"                % "1.0.0-RC21",
+  "dev.zio"                %% "zio-interop-cats"   % "2.1.3.0-RC16",
   "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
 )
+
+addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full)
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "fr.adelegue.controllers._"
